1f5c0e610239a3769b74ad172da612f181cc2d38
amangezahegn <amanuel.gezahegn578@gmail.com>
2026-08-03 23:19:02 +0300

    Hiding and showing form fields with state

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styles from "./TodoForm.module.css";
 
 export function TodoForm({ onCreate }) {
+  const [showAllFields, setShowAllFields] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -10,9 +13,10 @@ export function TodoForm({ onCreate }) {
 
     onCreate({
       name: elements.name.value,
-      description: elements.description.value,
-      deadline: elements.deadline.value,
-      priority: elements.priority.value,
+      description: elements.description?.value ?? "",
+      deadline: elements.deadline?.value ?? "",
+      priority: elements.priority?.value ?? "None",
+      completed: false,
     });
 
     event.target.reset();
@@ -20,7 +24,12 @@ export function TodoForm({ onCreate }) {
 
   return (
     <section>
-      <h3 className={styles.Title}>New To-Do</h3>
+      <h3 className={styles.Title}>
+        New To-Do
+        <button onClick={() => setShowAllFields(!showAllFields)}>
+          {showAllFields ? "Hide" : "Show"} all fields
+        </button>
+      </h3>
 
       <form className={styles.Form} onSubmit={handleSubmit}>
         <div className={styles.FormFields}>
@@ -33,34 +42,36 @@ export function TodoForm({ onCreate }) {
               autoComplete="off"
             />
           </div>
+          {showAllFields && (
+            <>
+              <div className={styles.FormField}>
+                <textarea
+                  aria-label="Description"
+                  placeholder="Description"
+                  name="description"
+                  rows="3"
+                />
+              </div>
 
-          <div className={styles.FormField}>
-            <textarea
-              aria-label="Description"
-              placeholder="Description"
-              name="description"
-              rows="3"
-            />
-          </div>
+              <div className={styles.FormGroup}>
+                <div className={styles.FormField}>
+                  <label htmlFor="deadline">Deadline</label>
+                  <input type="date" id="deadline" name="deadline" />
+                </div>
 
-          <div className={styles.FormGroup}>
-            <div className={styles.FormField}>
-              <label htmlFor="deadline">Deadline</label>
-              <input type="date" id="deadline" name="deadline" />
-            </div>
-
-            <div className={styles.FormField}>
-              <label htmlFor="priority">Priority</label>
-              <select defaultValue="none" id="priority" name="priority">
-                <option value="none">None</option>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-              </select>
-            </div>
-          </div>
+                <div className={styles.FormField}>
+                  <label htmlFor="priority">Priority</label>
+                  <select defaultValue="none" id="priority" name="priority">
+                    <option value="none">None</option>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                  </select>
+                </div>
+              </div>
+            </>
+          )}
         </div>
-
         <input type="submit" value="Add" />
       </form>
     </section>
