011d6d17afe00d7af17ebb61639aefe7ad0c8fb9
amangezahegn <amanuel.gezahegn578@gmail.com>
2026-08-04 21:57:40 +0300

    display todo list and color coded priorities

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import styles from "./App.module.css";
 import { TodoForm } from "./components/TodoForm/TodoForm";
 import { useState } from "react";
+import { TodoList } from "./components/TodoList/TodoList";
+
+const TODOS_DEFAULT = [
+  {
+    id: "1",
+    name: "Buy an Ice Cream",
+    description: "The white one with chocolate",
+    deadline: "2025-02-09",
+    priority: "low",
+    completed: false,
+  },
+  {
+    id: "2",
+    name: "Sell old MacBook Pro 2025",
+    description: "Try to sell it on OLX",
+    deadline: "2025-02-28",
+    priority: "high",
+    completed: false,
+  },
+  {
+    id: "3",
+    name: "Charge Powerbank",
+    description: "For the next travelling",
+    deadline: "2025-02-15",
+    priority: "medium",
+    completed: true,
+  },
+  {
+    id: "4",
+    name: "Test Todo onlye with a name",
+    description: "",
+    deadline: "",
+    priority: "none",
+    completed: false,
+  },
+];
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(TODOS_DEFAULT);
 
   function handleCreate(newTodo) {
     setTodos((prevTodos) => [
@@ -20,7 +56,7 @@ function App() {
 
       <div className={styles.AppContainer}>
         <TodoForm onCreate={handleCreate} />
-        {JSON.stringify(todos)}
+        <TodoList todos={todos} />
       </div>
     </div>
   );
---
+++ src/constants/priorities.js
@@ -0,0 +1,8 @@
+export const PRIORITIES = {
+  none: { label: "none", color: "" },
+  low: { label: "Low", color: "Teal" },
+  medium: { label: "Medium", color: "Gold" },
+  high: { label: "High", color: "Salmon" },
+};
+
+export const PRIORITY_DEFAULT = "none";
